a0627ea832ab54778b8f529916de1ea995ac56c8
Paulo Serrano <pvsserrano@gmail.com>
2023-07-11 19:31:53 -0300

    feat: categories ui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Categories from "./Categories";
 import Container from "./Container";
 import Logo from "./Logo";
 import Search from "./Search";
@@ -31,6 +32,7 @@ export default function Navbar({ currentUser }: NavbarProps) {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   )
 }
